81d9907d417c80c5e4da2691d320df49f41bb396
Henrique <papeschihenrique0@gmail.com>
2023-07-26 10:38:49 -0300

    Commit New

---
+++ Exercicios/Aula_29_30.py
@@ -0,0 +1,13 @@
+'''
+Henrique Papeschi tem 1.80 de altura
+pesa 80kg e seu IMC é:
+IMC = peso/ (altura x altura)
+'''
+
+nome = ('Henrique Papeschi')
+altura = 1.80
+peso = 80
+imc = peso / (altura * altura)
+
+print(nome, ' seu IMC é de ', imc)
+
