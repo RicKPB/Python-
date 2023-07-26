05780850c3dcac7478af90fbc9ff31ea9b425e31
Henrique <papeschihenrique0@gmail.com>
2023-07-26 15:58:53 -0300

    Commit Att

---
+++ Aulas/Aula_33/input.py
@@ -0,0 +1,13 @@
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome}')
+idade = int(input('Qual a sua idade: '))
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+
+print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
+
+print(f'A sua idade e: {idade}')
