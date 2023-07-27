c0a244997eefad02f6c7b25d494d21e837542f27
Henrique <papeschihenrique0@gmail.com>
2023-07-27 16:19:49 -0300

    Commit ADD

---
+++ Exercicios/Aula_54_a_57.py
@@ -0,0 +1,37 @@
+"""
+Faça um programa que peça ao usuário para digitar um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+
+# numero_digitado = input('Digite um numero: ')
+# resto = None
+
+# try:
+#     numero_int = int(numero_digitado)
+#     resto = numero_int % 2
+    
+#     if resto == 0:
+#         print(f'O numero {numero_int} é um numero par')
+    
+#     else:
+#         print(f'O numero {numero_int} é um numero impar')
+    
+
+# except:
+#     print(f'O numero que você informou não e um numero inteiro')
+
+
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
+
+
+
+"""
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
+"Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+"""
