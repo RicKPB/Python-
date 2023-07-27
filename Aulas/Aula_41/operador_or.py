a67d4c615c897ab2f548ed73c85d4379f74a97d3
Henrique <papeschihenrique0@gmail.com>
2023-07-27 11:25:48 -0300

    Commit ADD

---
+++ Aulas/Aula_41/operador_or.py
@@ -0,0 +1,35 @@
+# Operadores lógicos
+# and (e) or (ou) not (não)
+# or - Qualquer condição verdadeira avalia
+# toda a expressão como verdadeira.
+# Se qualquer valor for considerado verdadeiro,
+# a expressão inteira será avaliada naquele valor.(Sempre retorna o valor verdadeiro)
+# São considerados falsy (que vc já viu)
+# 0 0.0 '' False
+# Também existe o tipo None que é
+# usado para representar um não valor
+
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+
+# senha_permitida = '123456'
+
+# if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+#     print('Entrar')
+# else:
+#     print('Sair')
+
+# Avaliação de curto circuito
+senha = input('Senha: ') or 'Sem senha'
+print(senha)
+
+
+entrada = input('[E]ntrar ou [S]air: ')
+senha = input('Digite a senha: ')
+
+senha_permitida = '1234'
+if (entrada == 'E' or entrada == 'e') and senha == senha_permitida:
+    print('Entrar')
+    
+else :
+    print('sair')
---
+++ Aulas/Aula_46/fatiamento_str.py
@@ -0,0 +1,19 @@
+"""
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd 
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+print(variavel[::-1])
+print(variavel[4: 6: ])
+# Ordem     INICIO: FIM: PASSO
+
+print(10 * '-')
+
+print(len(variavel))
+print(len(variavel[4: 9]))
+#contagem de quantas char tem na str.
