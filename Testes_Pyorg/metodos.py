72444e3a8393807f0b683ccfa69c8dc588689665
Henrique <papeschihenrique0@gmail.com>
2023-08-01 16:37:23 -0300

    Commit ADD

---
+++ Testes_Pyorg/metodos.py
@@ -0,0 +1,16 @@
+nome = input('Digite seu nome: ')
+
+# O metodo .lower ele converte toda variavel str para minusculas mesmo e for escrita em maiscula
+nome = nome.lower()
+
+print(nome)
+
+#--------////------------////-----------////-----------////
+
+
+
+# O metodo .startswith ele serve para detectar se a variavel comeca com a letra determinada
+nome = nome.lower().startswith('s')
+
+#Ja o metodo .endswith ele serve para detectar se a variavel termina com a letra determinada
+nome = nome.lower().endswith()
