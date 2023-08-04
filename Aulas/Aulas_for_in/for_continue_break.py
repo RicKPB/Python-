6753919cb2775ba50d5b225cf2a132a30e180bd2
Henrique <papeschihenrique0@gmail.com>
2023-08-04 13:44:51 -0300

    Commit ADD

---
+++ Aulas/Aulas_for_in/for_continue_break.py
@@ -0,0 +1,18 @@
+'''
+whille == for
+'''
+
+for i in range(9):
+    if i == 2:
+        print('i e 2, pulando...')
+        continue
+    
+    if i == 8:
+        print('i e 8, seu else nao executara')
+        break
+    
+    for j in range(1, 3):
+        print(i, j)
+        
+else:
+    print('For completo com sucesso')
