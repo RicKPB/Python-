cbed4ba764b0d268abca5b9b9b4165fd3f16d2b0
Henrique <papeschihenrique0@gmail.com>
2023-08-09 15:06:25 -0300

    Commit Update past

--- Introducao_a_Python/Aulas/Aula_95/Interpretador_python.py
+++ Introducao_a_Python/Aulas/Aula_95/Interpretador_python.py
@@ -28,4 +28,4 @@ Embora nunca frequentemente seja melhor que *exatamente* agora.
 Se a implementação é difícil de explicar, é uma má ideia.
 Se a implementação é fácil de explicar, pode ser uma boa ideia.
 Namespaces são uma grande ideia -- vamos fazer mais dessas!
-"""
+""" 
