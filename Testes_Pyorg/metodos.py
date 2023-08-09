4ee0d25ea921ea34b9ec868811d89041f6322e71
Henrique <papeschihenrique0@gmail.com>
2023-08-09 13:39:29 -0300

    Commit ADD, EDIT

--- Testes_Pyorg/metodos.py
+++ Testes_Pyorg/metodos.py
@@ -5,7 +5,9 @@ nome = nome.lower()
 
 print(nome)
 
-#--------////------------////-----------////-----------////
+#---------/--------/--------/--------/--------/
+print('-' * 95)
+#---------/--------/--------/--------/--------/
 
 # O metodo .startswith ele serve para detectar se a variavel comeca com a letra determinada
 nome = nome.lower().startswith('s')
@@ -13,12 +15,20 @@ nome = nome.lower().startswith('s')
 #Ja o metodo .endswith ele serve para detectar se a variavel termina com a letra determinada
 # nome = nome.lower().endswith()
 
+#---------/--------/--------/--------/--------/
+print('-' * 95)
+#---------/--------/--------/--------/--------/
+
 # O metodo .upper deixa todas as letras da string em maiusculo.
 frase = 'O Python é uma linguagem de programação '\
     'multiparadigma. '\
     'Python foi criado por Guido van Rossum. '.upper()
     
 print(frase)
+
+#---------/--------/--------/--------/--------/
+print('-' * 95)
+#---------/--------/--------/--------/--------/
 
 # O metodo .count ele armazena a quantas vezes a palavra ou o numero apareceu no texto.
 
@@ -28,6 +38,9 @@ frase = 'O Python é uma linguagem de programação '\
     
 print(frase.count('Python'))
 
+#---------/--------/--------/--------/--------/
+print('-' * 95)
+#---------/--------/--------/--------/--------/
 
 # O metodo .append serve para voce adicionar um numero no final de uma lista
 
@@ -36,8 +49,22 @@ list = [10,20,30,40]
 list.append(50)
 print(list)
 
+#---------/--------/--------/--------/--------/
+print('-' * 95)
+#---------/--------/--------/--------/--------/
+
 # O metodo .pop serve para elminiar o ultimo elemento da lista
 list = [10,20,30]
 list.append(50)
 list.pop()
 list.pop(2) #remover sobre o indice
+
+#---------/--------/--------/--------/--------/
+print('-' * 95)
+#---------/--------/--------/--------/--------/
+
+# O metodo .strip() serve para apagra os espacos do comeco e do fim da frase.
+
+frase = '  Hello World  '
+print(frase.strip())
+# Em casos especificos tambem temos o rstrip(espaco a direita) e o lstrip(espaco a esquerda)
