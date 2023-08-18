f099be543b23dc4b4cb19e6cc02321532a6ce48d
RickPB <papeschihenrique0@gmail.com>
2023-08-18 14:45:58 -0300

    Commit UPDATE

--- Branch_Scr/imposto_salario.py
+++ Branch_Scr/imposto_salario.py
@@ -6,15 +6,15 @@ while True:
         print('Não paga imposto')
 
     elif salario_float >= 2000.01 and salario_float <= 3000.00:
-        imposto_8_porcento = (salario_float* 0.08) 
+        imposto_8_porcento = (salario_float - 2000.00) * 0.08 
         print(f'Deve pagar R$ {imposto_8_porcento:.2f} de imposto.')
 
     elif salario_float >= 3000.01 and salario_float <= 4500.00:
-        imposto_18_porcento = (salario_float * 0.18) 
+        imposto_18_porcento = (((salario_float - 2000.00) - 1000.00) * 0.18) + (1000.00 * 0.08)
         print(f'Deve pagar R$ {imposto_18_porcento:.2f} de imposto.')
 
     else:
-        imposto_28_porcento = (salario_float * 0.28)
+        imposto_28_porcento = ((((salario_float - 2000.00)-1000.00)-1500.00) * 0.28) + ((1000.00 *0.08)+(1500.00 * 0.18))
         print(f'Deve pagar R${imposto_28_porcento:.2f} de imposto')
         
     sair = input('Quer sair? [s]air ').lower().startswith('s')
