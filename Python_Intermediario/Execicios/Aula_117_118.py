80f749bfa35715baf1eaa6113628710e00ed7fcd
RicKPB <papeschihenrique0@gmail.com>
2023-09-16 14:36:50 -0300

    Commit UPDATE ALL

---
+++ Python_Intermediario/Execicios/Aula_117_118.py
@@ -0,0 +1,14 @@
+"""
+    Exercicios
+    Criar funcoes que duplicam, triplicam e quadruplicam
+    o numero rebido pelo parametro
+"""
+
+def main(valor):
+
+    valor = float(input('digite um valor '))
+    print(valor)
+
+
+
+main(10)
