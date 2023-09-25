ef4461b7d428fe72877ec55e9041bda37b7f3e94
RicKPB <papeschihenrique0@gmail.com>
2023-09-24 22:31:58 -0300

    Commit ADD exercicio_6.py NEW

---
+++ python_org/exercicio_6.py
@@ -0,0 +1,25 @@
+#Faça um programa que converta da notação de 24 horas para a notação de 12 horas. Por exemplo, o programa deve converter 14:25 em 2:25 P.M. A entrada é dada em dois inteiros. Deve haver pelo menos duas funções: uma para fazer a conversão e uma para a saída. Registre a informação A.M./P.M. como um valor ‘A’ para A.M. e ‘P’ para P.M. Assim, a função para efetuar as conversões terá um parâmetro formal para registrar se é A.M. ou P.M. Inclua um loop que permita que o usuário repita esse cálculo para novos valores de entrada todas as vezes que desejar.
+
+def coercaoHoras(*agrs):
+
+    while True:
+        
+        hora = int(input('Digite a hora: '))
+        minutos = int(input('Digite os minutos: '))
+        
+        if hora >= 13:
+            horario_A_M = hora - 12
+
+            print(f'{horario_A_M}:{minutos}')
+        
+        else:
+            print(f'{hora}:{minutos}')
+
+        #---------------SAIR SISTEMA------------------
+        sair = input('Deseja [S]air: ').upper().startswith('S')
+
+        if sair is True:
+            print('Sistema finalizado')
+            break
+
+coercaoHoras()
