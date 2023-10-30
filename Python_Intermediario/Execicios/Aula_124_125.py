3bba4d294ccc09ae251b0ed35bfae913c84f2537
RicKPB <papeschihenrique0@gmail.com>
2023-10-30 17:12:39 -0300

    Commit ADD, UPDATE /all, Codigo com problema que nao foi resolvido (analise ainda)

---
+++ Python_Intermediario/Execicios/Aula_124_125.py
@@ -0,0 +1,51 @@
+# Exercicio - sistema de perguntas e respostas
+
+perguntas = [ 
+    {
+        'Pergunta': 'Quanto é 2 + 2?',
+        'Opções': ['1', '3', '4', '5'],
+        'Resposta': '4',
+    },
+    {
+        'Pergunta': 'Quanto é 5 * 5?',
+        'Opções': ['25', '55', '10', '51'],
+        'Resposta': '25',
+    },
+    {
+        'Pergunta': 'Quanto é 10 / 2?',
+        'Opções': ['4', '5', '2', '1'],
+        'Resposta': '5',
+    }
+]
+
+for pergunta in perguntas:
+    print('Pergunta:', pergunta['Pergunta'])
+    print()
+
+    print('Opções:')
+    opcoes = pergunta['Opções']
+    for  i, opcoes in enumerate(opcoes):
+        
+        print(f'{i})',opcoes)
+    print()
+
+    escolha = input('Escolha uma opção: ')
+
+    acertou = False
+    escolha_int = None
+    qtd_opcoes =  len(opcoes)
+
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] ==  pergunta['Resposta']:
+                acertou = True
+            
+    if acertou:
+        print('Acertou!!')
+    else: 
+        print('Errou!')
+    
+    print()
